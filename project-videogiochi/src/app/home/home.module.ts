1b04ae07ad887b3dd76f642d0834393afdaf43a3
IzzoDev <aizzo450@gmail.com>
2024-06-22 23:19:56 +0200

    added footer

--- project-videogiochi/src/app/home/home.module.ts
+++ project-videogiochi/src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { HomeRoutingModule } from "./home-routing.module";
 import { CommonModule } from "@angular/common";
 import { NavbarComponent } from "../components/navbar/navbar.component";
 import { DashboardComponent } from "../components/dashboard/dashboard.component";
+import { FooterComponent } from "../components/footer/footer.component";
 
 @NgModule({
-    declarations:[HomeComponent,NavbarComponent,DashboardComponent],
+    declarations:[HomeComponent,NavbarComponent,DashboardComponent,FooterComponent],
     imports:[HomeRoutingModule,CommonModule],
     exports:[HomeComponent]
 })
