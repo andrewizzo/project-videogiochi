ee29684955fbd674e28f30e5f3a66e4c1e3f353f
an.izzo <an.izzo@almaviva.it>
2024-06-22 18:57:17 +0200

    added shared module

--- project-videogiochi/src/app/app.module.ts
+++ project-videogiochi/src/app/app.module.ts
@@ -3,25 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeModule } from './home/home.module';
-import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    DashboardComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HomeModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project-videogiochi/src/app/home/home.module.ts
+++ project-videogiochi/src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { HomeComponent } from "./home.component";
 import { HomeRoutingModule } from "./home-routing.module";
 import { CommonModule } from "@angular/common";
+import { NavbarComponent } from "../components/navbar/navbar.component";
 
 @NgModule({
-    declarations:[HomeComponent],
+    declarations:[HomeComponent,NavbarComponent],
     imports:[HomeRoutingModule,CommonModule],
     exports:[HomeComponent]
 })
